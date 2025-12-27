40186f67924bf7e01f0ff6ecd8e67f87c47494dc
Eugene Kim <yevkim@gmail.com>
2025-12-27 15:43:21 +0800

    reverse vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/kimchee-wedding/",
+  base: "./",
   server: {
     host: "0.0.0.0",
     port: 8080,
